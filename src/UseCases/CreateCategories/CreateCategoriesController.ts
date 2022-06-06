ec7dc3252436ccef159295293ca64504e8d6e497
ericDK89 <ericthr42@gmail.com>
2022-06-06 18:01:24 -0300

    Documentation  done

--- src/UseCases/CreateCategories/CreateCategoriesController.ts
+++ src/UseCases/CreateCategories/CreateCategoriesController.ts
@@ -2,13 +2,17 @@ import { Request, Response } from "express";
 import { CreateCategoriesUseCase } from "./CreateCategoriesUseCase";
 
 export class CreateCategoriesController {
-  constructor(private createCategoriesUseCase: CreateCategoriesUseCase) {}
+  constructor(private createCategoriesUseCase: CreateCategoriesUseCase) { }
 
   handle(req: Request, res: Response): Response {
-    const { name, description } = req.body;
 
-    this.createCategoriesUseCase.execute({ name, description });
+    try {
+      const { name, description } = req.body;
+      this.createCategoriesUseCase.execute({ name, description });
+      return res.status(201).send();
 
-    return res.status(201).send();
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
   }
 }
